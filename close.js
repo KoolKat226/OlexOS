76c80361e39a8dcf4b4e3dfc3aae5d51aaf147a1
KoolKat <99701844+KoolKat226@users.noreply.github.com>
2023-02-16 20:49:29 -0500

    Delete close.js

--- close.js
+++
@@ -1,6 +0,0 @@
-var iframe = document.createElement("iframe");
-iframe.setAttribute("style","position: fixed; z-index: 999999; height: 100vh; width: 100vw; top: 0px; border-radius: 0px; overflow: hidden; background-color: #fff;");
-iframe.setAttribute("id","olex");
-iframe.src = "https://koolkat226.github.io/OlexOS/";
-document.body.appendChild(iframe);
-iframe.onload=my_code;
