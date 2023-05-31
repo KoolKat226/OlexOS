efee299355905991e89320ff6db92c4220582e2c
KoolKat <99701844+KoolKat226@users.noreply.github.com>
2023-05-30 21:57:33 -0400

    Update doodlejump.js

--- loadapp/doodlejump.js
+++ loadapp/doodlejump.js
@@ -4,7 +4,7 @@ document.body.appendChild(div);
   div.innerHTML = `
 	<style type="text/css">
 		
-		.game {
+		.externalsoftware {
 			display: block;
   height: 100%;
 			border: none;
@@ -15,10 +15,9 @@ document.body.appendChild(div);
   height: 100%;
   margin-bottom: 0px;
   transition: 0.3s;
-z-index: 9993;
-background:rgba(217, 217, 217,.8); backdrop-filter: saturate(180%) blur(14px);
+z-index: 9992;
 		}
-  .gameoptions {
+  .softwareoptions {
     overflow: hidden;
   position: fixed;
   top: 0;
@@ -34,51 +33,24 @@ margin: auto;
             text-decoration: none;
           text-align:center;
         }
-
-        .gameclose {
-background:rgba(217, 217, 217,.2); backdrop-filter: saturate(180%) blur(14px);
-  overflow: hidden;
-  position: fixed;
-  bottom: 0;
-  width: 20%;
-  height: 30px;
-  z-index: 9999;
-  border-top-right-radius:20px;
-  border-top-left-radius:20px;
-transition: 0.3s;
-}
-
-.gameclose a {
-  float: left;
-  color: #f2f2f2;
-  text-align: center;
-  text-decoration: none;
-  font-size: 17px;
-transition: 0.3s;
-}
-
   
 	</style>
  
 </head>
 
-<div id="olexgame">
+<div id="externalsoftware">
 	<iframe src="https://thirdpartyapp.jacsyn.repl.co/.doodlejump.html"
 			frameborder="0"
 			width="100%"
 			height="100%"
       allowfullscreen
-			name="game"
-      id="game"
-      class="game">
+			name="orangeframe"
+      id="externalsoftware"
+      class="externalsoftware">
 </iframe>
 
 
-<div class="gameclose">
-<a style="cursor:pointer;">
-<img style="height:27px;position: relative; width:145px;" onclick="document.getElementById('olexgame').remove();" src="https://koolkat226.github.io/OlexOS/loadapp/closesoftware.png">
-</a>
-</div>
+
 
 
 
@@ -87,3 +59,25 @@ transition: 0.3s;
 
   `;
 document.body.appendChild(div);
+
+var style = document.createElement("style");
+document.body.appendChild(style);
+  style.innerHTML = `
+.external {
+display:block;
+}
+
+.hideexternal {
+display:none;
+}
+`;
+  document.body.appendChild(style);
+
+var closebutton = document.getElementById("externalapp");
+closebutton.style.display = "block";
+
+var applibrary = document.getElementById("mainapps");
+applibrary.style.display = "none";
+
+var text = document.getElementById("hideappstext");
+text.style.display = "block";
